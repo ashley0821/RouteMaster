b7b4021fd645c9e111eee54d8d525d43e5bde7cc
LAPTOP-9EALKCFB\user <blizzard255324@gmail.com>
2023-06-22 22:58:30 +0800

    我的住宿列表(三層式架構)

--- RouteMaster/Models/Services/AccommodationService.cs
+++ RouteMaster/Models/Services/AccommodationService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RouteMaster.Models.Dto;
+using RouteMaster.Models.Interfaces;
+using RouteMaster.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +10,16 @@ namespace RouteMaster.Models.Services
 {
 	public class AccommodationService
 	{
+		public IAccommodationRepository _repo { get; set; }
+
+		public AccommodationService(IAccommodationRepository repo)
+		{
+			this._repo = repo;
+		}
+
+		public IEnumerable<AccommodationIndexDto> Search()
+		{
+			return _repo.Search();
+		}
 	}
 }
