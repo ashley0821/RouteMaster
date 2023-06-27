a894cf07868009c2779f372235ceca7dffa205f1
ian <sd180911@gmail.com>
2023-06-27 15:32:41 +0800

    套裝行程亂寫的大概架構

---
+++ RouteMaster/Models/Infra/DapperRepositories/PackageTourDapperRepository.cs
@@ -0,0 +1,33 @@
+﻿using RouteMaster.Models.Dto;
+using RouteMaster.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RouteMaster.Models.Infra.DapperRepositories
+{
+
+    public class PackageTourDapperRepository : IPackageTourRepository
+    {
+        public void Create(PackageTourCreateDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(PackageTourEditDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PackageTourIndexDto> Search()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RouteMaster/Models/Services/PackageTourService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RouteMaster.Models.Services
+{
+    public class PackageTourService
+    {
+
+
+    }
+}
