2bef18366aa384025d3cfd503042b7ac33547713
kristy <s965402323@gmail.com>
2023-06-27 20:48:36 +0800

    6/27
    ActivityDetail
    [V]add ActivitiesDetailsDto, ActivitiesCreateDetailsDto, ActivitiesEditDetailsDto
    [V]add Dapper connection in DapperRepositories(search only for testing), edit, create, delete to do..
    [V]add ActivitiesDetailsIndexVM
    [V]add Dapperfunction in Controllers

---
+++ RouteMaster/Controllers/ActivitiesDetailsController.cs
@@ -0,0 +1,40 @@
+﻿using RouteMaster.Models.EFModels;
+using RouteMaster.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RouteMaster.Controllers
+{
+	public class ActivitiesDetailsController : Controller
+	{
+		// GET: ActivitiesDetails
+
+		private readonly AppDbContext db = new AppDbContext();
+		public ActionResult IndexDapper()
+		{
+
+
+			//var ActivitiesDetailsItems = new ActivitiesDetailsDapperRepository().GetActivitiesDetails();
+			var test = db.ActivitiesDetails;
+
+			var viewModelItems = test
+				.ToList()
+				.Select(dto => new ActivitiesDetailsIndexVM
+				{
+					Id = dto.Id,
+					OrderId = dto.OrderId,
+					ActivityId = dto.ActivityId,
+					ActivityName = dto.ActivityName,
+					StartTime = dto.StartTime,
+					EndTime = dto.EndTime,
+					Price = dto.Price,
+					Quantity = dto.Quantity,
+
+				});
+			return PartialView(viewModelItems);
+		}
+	}
+}
