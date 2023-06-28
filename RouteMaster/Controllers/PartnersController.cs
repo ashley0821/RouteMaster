81cce6cddb4fd68f6600dfae48cb8e911ccfbae4
Ben Wang <fp4652@yahoo.com.tw>
2023-06-29 00:12:38 +0800

    建立Partners 以及 administrators 的crud

---
+++ RouteMaster/Controllers/PartnersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RouteMaster.Models.EFModels;
+
+namespace RouteMaster.Controllers
+{
+    public class PartnersController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Partners
+        public ActionResult Index()
+        {
+            return View(db.Partners.ToList());
+        }
+
+        // GET: Partners/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Partner partner = db.Partners.Find(id);
+            if (partner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(partner);
+        }
+
+        // GET: Partners/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Partners/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Email,EncryptedPassword,CreateDate,IsConfirmed,ConfirmCode,IsSuspended")] Partner partner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Partners.Add(partner);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(partner);
+        }
+
+        // GET: Partners/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Partner partner = db.Partners.Find(id);
+            if (partner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(partner);
+        }
+
+        // POST: Partners/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Email,EncryptedPassword,CreateDate,IsConfirmed,ConfirmCode,IsSuspended")] Partner partner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(partner).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(partner);
+        }
+
+        // GET: Partners/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Partner partner = db.Partners.Find(id);
+            if (partner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(partner);
+        }
+
+        // POST: Partners/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Partner partner = db.Partners.Find(id);
+            db.Partners.Remove(partner);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
