f4aa550911bdb0d34b277b5b769742a451f8e330
kristy <s965402323@gmail.com>
2023-06-29 20:52:28 +0800

    6/29 8:51 AccomodationDetails partial view (useDapper) [v] add Dto [v] add interface, service [v] add DapperRepository [X] need to fix partial view bug

--- ReadMeOrder1.txt
+++ ReadMeOrder1.txt
@@ -5,4 +5,11 @@ ExtraServicesDetails partial view
 [v] add ExtraServicesDetailsVM
 [v] add ExtraServicesDetails function on Ordercontroller >>partial view
 
-[.]fix the bug
+[v]fix the extraservicedetails bug
+
+6/29 8:51
+AccomodationDetails partial view (useDapper)
+[v] add Dto
+[v] add interface, service
+[v] add DapperRepository
+[X] need to fix partial view bug 
---
+++ RouteMaster/Models/Interfaces/IAccomodationDetailsRepository.cs
@@ -0,0 +1,23 @@
+﻿using RouteMaster.Models.Dto;
+using RouteMaster.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteMaster.Models.Interfaces
+{
+	public interface IAccomodationDetailsRepository
+	{
+		
+			IEnumerable<AccommodationDetailsDto> Search(AccomodationDetailsVM vm);
+
+			void Create(AccommodationDetailsDto dto);
+
+			void Edit(AccommodationDetailsDto dto);
+
+			void Delete(int id);
+		
+	}
+}
