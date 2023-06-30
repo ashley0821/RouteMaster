f7c377c91612ec1b31ffaa11b94551c0ade2cf93
Ashley Chen <noone183492761@gmail.com>
2023-06-30 20:40:06 +0800

    20230630

---
+++ RouteMaster/Models/Dto/AttractionDetailDto.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RouteMaster.Models.Dto
+{
+	public class AttractionDetailDto
+	{
+		public int Id { get; set; }
+
+		public string Category { get; set; }
+
+		public string Region { get; set; }
+
+		public string Town { get; set; }
+
+		public string Name { get; set; }
+
+		public string Address { get; set; }
+
+		public double? PositionX { get; set; }
+
+		public double? PositionY { get; set; }
+
+		public string Description { get; set; }
+
+		public string Website { get; set; }
+
+		
+
+		public double? AverageScore { get; set; }
+
+		public string AverageScoreText
+		{
+			get
+			{
+				if (AverageScore == null) { return "尚未有評分"; }
+				else { return AverageScore.Value.ToString(); }
+			}
+		}
+
+		public double? AverageStayHours { get; set; }
+
+		public string AverageStayHoursText
+		{
+			get
+			{
+				if (AverageStayHours == null) { return "尚未有資料"; }
+				else { return AverageStayHours.Value.ToString(); }
+			}
+		}
+
+		public int? AveragePrice { get; set; }
+
+		public string AveragePriceText
+		{
+			get
+			{
+				if (AveragePrice == null) { return "尚未有資料"; }
+				else { return AveragePrice.Value.ToString(); }
+			}
+		}
+	}
+}
