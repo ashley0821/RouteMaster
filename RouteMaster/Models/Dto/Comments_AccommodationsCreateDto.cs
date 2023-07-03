4658bf49fb03c07fcbf58a39698552462ca5e452
mopekim <vergalsaver78@gmail.com>
2023-07-04 00:32:17 +0800

    完成Comments_AccommodationsCreate三層式code，Create ViewPage待修改

--- RouteMaster/Models/Dto/Comments_AccommodationsCreateDto.cs
+++ RouteMaster/Models/Dto/Comments_AccommodationsCreateDto.cs
@@ -5,26 +5,20 @@ using System.Linq;
 using System.Web;
 using System.Xml.Linq;
 
-namespace RouteMaster.Models.ViewModels
+namespace RouteMaster.Models.Dto
 {
-	public class Comments_AccommodationsCreateVM
+	public class Comments_AccommodationsCreateDto
 	{
-		[Display(Name = "帳號名稱")]
 		public string MemberAccount { get; set; }
 
-		[Display(Name = "住宿名稱")]
 		public int AccomodationId { get; set; }
 
-		[Display(Name = "評論標題")]
 		public string Title { get; set; }
 
-		[Display(Name = "優點")]
 		public string Pros { get; set; }
 
-		[Display(Name = "缺點")]
 		public string Cons { get; set; }
 
-		[Display(Name = "評分")]
 		public float Score { get; set; }
 
 	}
