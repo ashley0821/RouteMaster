36d5a39164fd1c5f82392ba8c7728cb12f289fd1
Yeh420 <blizzard255324@gmail.com>
2023-07-04 21:03:54 +0800

    ServiceInfoEdit OK

---
+++ RouteMaster/Models/ViewModels/Accommodations/ServiceInfoVM.cs
@@ -0,0 +1,15 @@
+﻿using RouteMaster.Models.Dto.Accommodation.Service;
+using RouteMaster.Models.EFModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RouteMaster.Models.ViewModels.Accommodations
+{
+	public class ServiceInfoVM
+	{
+		public int AccommodationId { get; set; }
+		public List<ServiceInfoDto> ServiceInfoList { get; set; }
+	}
+}
