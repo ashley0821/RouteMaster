526ff1fd1fe8bc3ab26fb1f92748bceac73f064a
mopekim <vergalsaver78@gmail.com>
2023-07-06 00:38:30 +0800

    建立Comments_AccommodationCriteria，修改FAQIndex樣式

---
+++ RouteMaster/Models/Infra/Criterias/Comments_AccommodationCriteria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RouteMaster.Models.Infra.Criterias
+{
+	public class Comments_AccommodationCriteria
+	{
+        public string AccomodationName { get; set; }    
+        public string Title { get; set; }
+
+        public int? SortId { get; set; } 
+    }
+}
