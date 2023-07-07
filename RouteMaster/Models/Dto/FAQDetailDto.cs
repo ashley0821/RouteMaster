d2401a69f2555edd2eb3a608345d88924209b8a1
mopekim <vergalsaver78@gmail.com>
2023-07-08 01:16:31 +0800

    FAQDetail功能完成

---
+++ RouteMaster/Models/Dto/FAQDetailDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RouteMaster.Models.Dto
+{
+	public class FAQDetailDto
+	{
+		public int Id { get; set; }
+
+		public string CategoryName { get; set; }
+
+		public string Question { get; set; }
+
+		public string Answer { get; set; }
+
+		public int Helpful { get; set; }
+
+		public IEnumerable<string> Images { get; set; }
+	}
+}
