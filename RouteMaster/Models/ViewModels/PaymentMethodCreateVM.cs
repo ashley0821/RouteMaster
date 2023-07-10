39132b7b7226709849f0f3cb110a669148c535e6
kristy <s965402323@gmail.com>
2023-07-10 14:41:48 +0800

    123

--- RouteMaster/Models/ViewModels/PaymentMethodCreateVM.cs
+++ RouteMaster/Models/ViewModels/PaymentMethodCreateVM.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Xml.Linq;
 
 namespace RouteMaster.Models.ViewModels
 {
 	public class PaymentMethodCreateVM
 	{
+		[Display(Name = "訂單編號")]
 		public int id { get; set; }
+		[Display(Name = "付款方式")]
 		public string Name { get; set; }
+		[Display(Name = "描述")]
 		public string Description { get; set; }
 	}
 }
